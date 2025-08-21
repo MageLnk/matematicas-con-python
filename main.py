340ff1295d481521d4266689ab64373c2f69dbeb
alumnos <alumnos@AVAWSF02LC0128.duoc.acad>
2025-08-20 20:45:19 -0400

    Clase 02

--- main.py
+++ main.py
@@ -17,7 +17,14 @@ while repeatCicle == True:
     print("Elije la opción que deseas experimentar: ")
     print("1. Calcular la altura promedio de un niño entre 0 y 12 meses de vida.")
     print("2. Calcular la dosis de paracetamol para niños de 1 a 12 años.")
+
+    print("0. Salir del programa.")
     selectOption = int(input("Elija su opción. "))
+
+    if selectOption == 0:
+        print("Gracias por usar el programa. ¡Hasta luego!")
+        exit()
+
     selectedOptionFunction = mathFunctionMapping.get(selectOption, optionNotAvailable)
     selectedOptionFunction()
     
